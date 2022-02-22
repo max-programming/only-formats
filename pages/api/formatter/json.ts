854ffd57e49c46784760afd3e765ccb2fdc60d5b
Max Programming <ui.sabuwala@gmail.com>
2022-02-22 19:31:43 +0530

    ✨ JSON API route

---
+++ pages/api/formatter/json.ts
@@ -0,0 +1,35 @@
+import formatCode from '@/utils/formatCode';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { Options } from 'prettier';
+import jsonParser from 'prettier/parser-babel';
+
+interface Data {
+  unformatted: string;
+  formatted: string;
+  options: Options;
+  msg: 'success' | 'error';
+}
+
+interface Body {
+  unformatted: string;
+}
+
+export default function jsFormat(
+  req: NextApiRequest,
+  res: NextApiResponse<Partial<Data>>
+) {
+  if (req.method !== 'POST') return res.status(401).send({ msg: 'error' });
+  const { unformatted }: Body = req.body;
+  const formatted = formatCode({
+    code: unformatted,
+    language: 'json',
+    parser: jsonParser,
+  });
+  res.send({
+    unformatted,
+    formatted,
+    msg: formatted === 'Invalid code' ? 'error' : 'success',
+  });
+}
