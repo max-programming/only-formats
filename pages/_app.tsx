bf601fbcff7dfccf5ffbcde90d6acea49777c205
Max Programming <51731966+max-programming@users.noreply.github.com>
2022-02-24 13:34:29 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AnimatePresence } from 'framer-motion';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AnimatePresence
         exitBeforeEnter
         initial={false}
