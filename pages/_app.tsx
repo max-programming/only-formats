fc030249d76893c36d2cd07ce4f2aa7b6f5bd012
Max Programming <ui.sabuwala@gmail.com>
2022-02-25 18:04:11 +0530

    ✨ Added PHP format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,27 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AnimatePresence } from 'framer-motion';
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import NextNProgress from 'nextjs-progressbar';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import Prism from 'prism-react-renderer/prism';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
-}
+};
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // @ts-ignore
+    (typeof global !== 'undefined' ? global : window).Prism = Prism;
+    import('prismjs/components/prism-php');
+  }, []);
   return (
     <ChakraProvider theme={theme}>
+      <NextNProgress options={{ showSpinner: false }} />
       <AnimatePresence
         exitBeforeEnter
         initial={false}
---
+++ pages/formatter/php.tsx
@@ -0,0 +1,83 @@
+import Layout from '@/components/Layout';
+import type { NextPage } from 'next';
+import Setting from '@/components/Setting';
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Switch,
+} from '@chakra-ui/react';
+import usePath from '@/utils/usePath';
+import formatCode from '@/utils/formatCode';
+
+import phpParser from '@prettier/plugin-php/standalone';
+import { useState } from 'react';
+import Formatter from '@/components/Formatter';
+import Header from '@/components/Header';
+import { OnChange } from '@monaco-editor/react';
+
+const PHPFormat: NextPage = () => {
+  const path = usePath('json');
+  const [indent, setIndent] = useState(2);
+  const [unformatted, setUnformatted] = useState('');
+  const [formatted, setFormatted] = useState('');
+
+  const format = ({
+    val,
+    indentation,
+  }: {
+    val?: string;
+    indentation?: number;
+  }) => {
+    setFormatted(
+      formatCode({
+        code: val || unformatted,
+        language: 'php',
+        parser: phpParser,
+        options: { tabWidth: indentation || indent },
+      })
+    );
+  };
+
+  const handleChange: OnChange = val => {
+    if (!val) return;
+    setUnformatted(val);
+    if (val.trim() === '') return setFormatted('');
+    format({ val, indentation: indent });
+  };
+
+  return (
+    <>
+      <Layout title={path.title}>
+        <Header title={path.title} imageSrc={path.image} />
+        <Setting title='Indentation'>
+          <NumberInput
+            onChange={(_, val) => {
+              setIndent(val);
+              format({ indentation: val });
+            }}
+            defaultValue={indent}
+            min={1}
+            max={10}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </Setting>
+        <Formatter
+          language={'php'}
+          unformattedCode={unformatted || ''}
+          formattedCode={formatted || ''}
+          handleChange={handleChange}
+        />
+      </Layout>
+    </>
+  );
+};
+
+export default PHPFormat;
