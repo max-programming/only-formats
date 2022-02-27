60d56b4c3c0797beeae9bd743ddbc5834b4f8672
Max Programming <ui.sabuwala@gmail.com>
2022-02-27 11:46:39 +0530

    💄 Fix card width and height

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,43 +15,49 @@ const Home: NextPage = () => {
   return (
     <>
       <Layout title='OnlyFormats'>
-        <SimpleGrid mt='16' columns={[1, 2, 3, 4]} spacing={5}>
+        <SimpleGrid
+          mt='16'
+          columns={[1, 2, 3, 4]}
+          spacing={3}
+          placeItems='center'
+          h='75vh'
+        >
           {paths.map(path => (
-            <GridItem key={path.id}>
-              <Link href={path.id}>
-                <a>
-                  <Center>
-                    <Box
-                      maxW='sm'
-                      transition='all 0.3s ease'
-                      borderWidth='2px'
-                      borderRadius='lg'
-                      overflow='hidden'
-                      _hover={{
-                        borderColor: 'teal.100',
-                        transform: 'translateY(-10px)',
-                        bg: 'blackAlpha.300',
-                      }}
-                    >
-                      <Image
-                        rounded={'lg'}
-                        height={230}
-                        width={282}
-                        objectFit={'scale-down'}
-                        mx='auto'
-                        src={path.image}
-                        alt=''
+            <Link href={path.id} key={path.id}>
+              <a>
+                <Center>
+                  <Box
+                    maxW='72'
+                    transition='all 0.3s ease'
+                    borderWidth='2px'
+                    borderRadius='lg'
+                    overflow='hidden'
+                    _hover={{
+                      borderColor: 'teal.100',
+                      transform: 'translateY(-10px)',
+                      bg: 'blackAlpha.300',
+                    }}
+                  >
+                    <Image
+                      rounded={'lg'}
+                      height={230}
+                      width={282}
+                      objectFit={'scale-down'}
+                      mx='auto'
+                      src={path.image}
+                      alt=''
+                    />
+                    <Box p={6}>
+                      <Heading
+                        textAlign='center'
+                        size='md'
+                        dangerouslySetInnerHTML={{ __html: path.title }}
                       />
-                      <Box p={6}>
-                        <Heading textAlign='center' size='md'>
-                          {path.title}
-                        </Heading>
-                      </Box>
                     </Box>
-                  </Center>
-                </a>
-              </Link>
-            </GridItem>
+                  </Box>
+                </Center>
+              </a>
+            </Link>
           ))}
         </SimpleGrid>
       </Layout>
